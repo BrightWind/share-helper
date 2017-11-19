61edd91a577d72208ad44a70a6304623e639d756
brightwind <653158302@qq.com>
2017-11-20 00:31:43 +0800

    project initialize

---
+++ src/main/java/mark/workshop/com/sharehelper/ShareHelperApplication.java
@@ -0,0 +1,12 @@
+package mark.workshop.com.sharehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareHelperApplication.class, args);
+	}
+}
---
+++ src/test/java/mark/workshop/com/sharehelper/ShareHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package mark.workshop.com.sharehelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShareHelperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
